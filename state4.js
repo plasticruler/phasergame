f7e998739932556a99e9dbb1d3b9b8298ce9a87b
marmite@dollar <mailbox@a20.co.za>
2018-10-11 21:46:15 +0200

    load cannon configuration data from config, load track from config, rotate cannon to balloon and shoot (varying firing rate)

--- state4.js
+++ state4.js
@@ -1,7 +1,8 @@
 var cursors, velocity = 500,
-    sky, floor, fx, text, cannon,balloon;
-var balloons = [], cannons = [],
-    tween;
+    sky, floor, fx, text, cannon, balloon;
+var balloons = [],
+    cannons = [],fireRate =500, nextFire=0,
+    tween,bullets, cannonConfigurations;
 
 var MILLISECONDS_TO_TRAVERSE_PATH = 10000;
 demo.state4 = function () {};
@@ -13,8 +14,9 @@ demo.state4.prototype = {
         game.load.spritesheet('balloonsprite', './assets/spritesheets/balloons.png', 82, 120);
         game.load.audio('plop', './assets/audio/plop.wav');
         game.load.spritesheet('explosion', './assets/spritesheets/explosion.png', 64, 64);
-        game.load.json('track1', './assets/tracks/track1.json');
-        game.load.spritesheet('tw-assets','./assets/spritesheets/tw-assets.png',100,100);
+        game.load.json('track1', './assets/data/track1.json');
+        game.load.json('gamedata','./assets/data/game-data.json');
+        game.load.spritesheet('tw-assets', './assets/spritesheets/tw-assets.png', 100, 100);        
     },
     create: function () {
         game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -23,13 +25,12 @@ demo.state4.prototype = {
         game.stage.backgroundColor = '#DDDDDD';
         game.world.setBounds(0, 0, 2813, 1000);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-      
-
         game.input.mouse.capture = true;
+        
         fx = game.add.audio('plop');
         fx.addMarker('plop', 0, 0.468);
-
         fx.allowMultiple = true;
+       
         var style = {
             font: "20px Arial",
             fill: "#000000",
@@ -37,21 +38,30 @@ demo.state4.prototype = {
         };
         text = game.add.text(200, 50, `Use tween to make a sprite follow a path loaded from a JSON file. Also place cannons by configuration data.`, style);
         text.anchor.set(0.5);
-        var levelData = game.cache.getJSON('track1');
+        
         balloon = game.add.sprite(100, 100, 'balloonsprite');
         balloon.anchor.setTo(0.5, 0.5);
-        console.log(levelData);
-        levelData.cannonLocations.forEach(function(l){
-            var c = game.add.sprite(l.position.x, l.position.y, 'tw-assets');
-            c.frame=0;
-            c.anchor.setTo(0.5, 0.5);
-            c.scale.setTo(1, 1);    
-            cannons.push(c);
-        });
-
         balloon.events.onInputDown.add(this.setBalloonToDestroyItself, this, 1);
 
-        cursors = game.input.keyboard.createCursorKeys();        
+        var levelData = game.cache.getJSON('track1');
+        var gameData = game.cache.getJSON('gamedata');
+        console.log(levelData);
+        cannonConfigurations = gameData.cannonConfigurations;
+        console.log(cannonConfigurations);
+        levelData.cannonLocations.forEach(function (l) {
+            var c = game.add.sprite(l.position.x, l.position.y, 'tw-assets');
+            cannonConfig = cannonConfigurations.filter(function(c){return c.id==l.id})[0];            
+            c.weaponId = l.id;
+            c.nextFire = 0;
+            c.frame = cannonConfig.spriteId;
+            c.nextFire = 0; //ready to fire immediately
+            c.anchor.setTo(0.5, 0.5);
+            c.scale.setTo(1, 1);
+            c.range = l.range;
+            cannons.push(c);
+        });       
+        
+        cursors = game.input.keyboard.createCursorKeys();
 
         tween = game.add.tween(balloon).to({
             x: levelData.path.map(function (x) {
@@ -73,7 +83,7 @@ demo.state4.prototype = {
 
         if (levelData.path.length > 0)
             graphics.moveTo(levelData.path[0].x, levelData.path[0].y);
-            levelData.path.forEach(function (x) {
+        levelData.path.forEach(function (x) {
             graphics.beginFill('#fefefe', 0.5);
             graphics.lineStyle(2, '#fefefe', 1);
             graphics.lineTo(x.x, x.y);
@@ -94,11 +104,24 @@ demo.state4.prototype = {
         ex.animations.add('explosion');
         ex.play('explosion', 16, true);
         game.time.events.add(Phaser.Timer.SECOND * 1, this.killSprite, this, ex);
-        sprite.destroy();
     },
     killSprite: function (sprite) {
         sprite.destroy();
-    },   
+    },
+    fire: function(cannon,target){
+        cannonConfig = cannonConfigurations.filter(function(c){return c.id==cannon.weaponId})[0];
+        if (game.time.now > cannon.nextFire){            
+            cannon.nextFire = game.time.now + cannonConfig.fireRate*1000;            
+            var w = game.add.weapon(1,'tw-assets',cannonConfig.spriteId);
+            w.bullets.setAll('scale.x',cannonConfig.scaleX);
+            w.bullets.setAll('scale.y',cannonConfig.scaleY);
+            w.trackSprite(cannon,0,0,true); //basically from where to shoot with an offset for the turret
+            w.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+            w.fireRate = 1000/cannonConfig.fireRate; //every so many seconds can shoot
+            w.bulletSpeed = cannonConfig.weaponSpeed;
+            w.fireAtXY(target.x,target.y);
+        }
+    },
     update: function () {
         for (var i = 0; i < balloons.length; i++) {
             if (balloons[i].y - balloons[i].height / 2 <= 0 && !balloons[i].isDead) {
@@ -106,15 +129,14 @@ demo.state4.prototype = {
                 balloons[i].isDead = true;
             }
         }
-
-
-        cannons.forEach(function(c){
-            if (game.physics.arcade.distanceBetween(balloon, c) < 200)
-            c.rotation = game.physics.arcade.angleBetween(c,balloon)+1.6;
-        });
-         
-        //https://www.codecaptain.io/blog/game-development/shooting-bullets-using-phaser-groups/518
-        //bullets now
+        cannons.forEach(function (c) {
+            cannonConfig = cannonConfigurations.filter(function(cn){return c.weaponId==cn.id})[0];
+            if (game.physics.arcade.distanceBetween(balloon, c) < cannonConfig.range){
+                c.rotation = game.physics.arcade.angleBetween(c, balloon) + 1.6;
+                this.fire(c,balloon);
+            }
+        },this);
+        
     }
 }
 
